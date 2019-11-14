d54bd738a2b3732064e08c956d68d63eb2a7898d
b4 <b4@Ter74-W10.cen.local>
2019-11-13 21:33:03 -0300

    primera version del himno:)

---
+++ Himno/src/himno/Himno.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package himno;
+
+/**
+ *
+ * @author b4
+ */
+public class Himno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Oid Mortales");
+        System.out.println("el grito sagrado");
+        System.out.println("Oid el ruido de rotas cadenas");
+        System.out.println("ved el trono a la noble repulgue");
+        System.out.println("ya a su trono dignisimo cerraron");
+        System.out.println("las provincias unidas de Europa");
+        System.out.println("y los libres del mundo responden");
+        System.out.println("al gran pueblo argentino salud.");
+        System.out.println("sean eternos los laureles");
+        System.out.println("que supimos conseguir");
+        System.out.println("sean eternos los laureles");
+        System.out.println("que supimos conseguir");
+        System.out.println("coronados de gloria");
+        System.out.println(" o juremos con gloria morir");
+        System.out.println(" o juremos con gloria morir");
+        System.out.println(" o juremos con gloria morir");
+
+        // TODO code application logic here
+    }
+
+}
