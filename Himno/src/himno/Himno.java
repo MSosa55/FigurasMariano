57ffffb7a9774d82a56191d93ad4858414c2cbba
b4 <b4@Ter74-W10.cen.local>
2019-11-13 21:57:36 -0300

    segunda version del himno

--- Himno/src/himno/Himno.java
+++ Himno/src/himno/Himno.java
@@ -17,22 +17,25 @@ public class Himno {
     public static void main(String[] args) {
         System.out.println("Oid Mortales");
         System.out.println("el grito sagrado");
+        System.out.println("libertad, libertad, libertad");
         System.out.println("Oid el ruido de rotas cadenas");
-        System.out.println("ved el trono a la noble repulgue");
-        System.out.println("ya a su trono dignisimo cerraron");
-        System.out.println("las provincias unidas de Europa");
+        System.out.println("ved el trono a la noble igualdad");
+        System.out.println("ya a su trono dignisimo abrieron");
+        System.out.println("las provincias unidas del sud");
         System.out.println("y los libres del mundo responden");
         System.out.println("al gran pueblo argentino salud.");
+        System.out.println("y los libres del mundo responden");
+        System.out.println("al gran pueblo argentino salud");
         System.out.println("sean eternos los laureles");
         System.out.println("que supimos conseguir");
         System.out.println("sean eternos los laureles");
         System.out.println("que supimos conseguir");
-        System.out.println("coronados de gloria");
+        System.out.println("coronados de gloria vivamos");
         System.out.println(" o juremos con gloria morir");
         System.out.println(" o juremos con gloria morir");
         System.out.println(" o juremos con gloria morir");
 
-        // TODO code application logic here
+       
     }
 
 }
