10f2bf004534352e6079b62cfb1ab66d302af077
b4 <b4@Ter74-W10.cen.local>
2019-11-14 21:52:05 -0300

    primera versiòn

---
+++ ColoresTuNombre1/src/ColoresMariano.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author b4
+ */
+public class ColoresMariano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String color1, color2, color3;
+        color1 = "AZUL";
+        color2  = "ROJO";
+        color3  = "VERDE";
+        System.out.printf("% es mi primer color favorito\n", color1);
+        System.out.printf("%es mi segundo color favorito\n", color2);
+        System.out.printf("%es mi tercer color favorito\n", color3);
+        // TODO code application logic here
+    }
+    
+}
